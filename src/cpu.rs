bc124d5f0a39419ce17900c36e2bc0b93fe63af2
Elodie Vandenberghe <elodie.vandenberghe@outlook.com>
2025-10-25 16:10:47 +0200

    Implementing ADD instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -56,3 +56,33 @@ impl std::convert::From<u8> for FlagRegister {
         }
     }
 }
+
+enum Instruction {
+    ADD(ArithmeticTarget),
+}
+
+enum ArithmeticTarget {
+    A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADD(target) => {
+                match target {
+                    ArithmeticTarget::C => {
+                        // TODO: implement ADD on register C
+                    }
+                    _ => { /* TODO: support more targets */ }
+                }
+            }
+            _ => { /* TODO: support more instructions */ }
+        }
+    }
+}
