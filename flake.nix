4d90360948945b93c0b9972224008bd6ec9f47f2
Elodie Vandenberghe <elodie.vandenberghe@outlook.com>
2025-11-03 21:47:40 +0100

    Seperatign memory bus, registers, and cpu instructions into seperate files

--- flake.nix
+++ flake.nix
@@ -5,17 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let 
+  outputs =
+    { self, nixpkgs }:
+    let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; }; 
-    in {
-       devShells.${system}.default = pkgs.mkShell {
-         buildInputs = with pkgs; [
-            cargo rustc rustfmt rustPackages.clippy rust-analyzer
-          ];
-       };
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          rustPackages.clippy
+          rust-analyzer
+        ];
+      };
     };
 
-  }
-
+}
