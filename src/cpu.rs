7f11e9ab345aaf0c57e54fe39f43aa2ec48698f7
Elodie Vandenberghe <elodie.vandenberghe@outlook.com>
2025-11-15 20:26:54 +0100

    ADD and SUB instructions tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -46,10 +46,11 @@ impl Cpu {
         let r = a.wrapping_sub(value).wrapping_sub(c);
         self.registers.set_flag(CpuFlags::Z, r == 0);
         self.registers
-            .set_flag(CpuFlags::H, ((a & 0xF) < (value & 0xF) + c));
+            .set_flag(CpuFlags::H, (a & 0xF) < (value & 0xF) + c);
         self.registers.set_flag(CpuFlags::N, true);
         self.registers
-            .set_flag(CpuFlags::C, (a as u16) < (value as u16 & 0xF) + c as u16);
+            .set_flag(CpuFlags::C, (a as u16) < (value as u16 + c as u16));
+        self.registers.a = r;
     }
 
     pub fn execute(&mut self, opcode: u8) -> u16 {
@@ -256,5 +257,280 @@ mod cpu_tests {
         assert_eq!(cpu.registers.a, 0xFE);
         assert_eq!(cpu.registers.get_flag(CpuFlags::C), true);
         assert_eq!(cpu.registers.get_flag(CpuFlags::H), true);
+    } // ADD A, C
+    #[test]
+    fn test_add_c_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x10;
+        cpu.registers.c = 0x20;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x81);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x30);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    #[test]
+    fn test_add_c_to_a_hc_flag() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0xF;
+        cpu.registers.c = 0x1;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x81);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x10);
+        assert_eq!(cpu.registers.f, 0b00100000);
+    }
+
+    #[test]
+    fn test_add_c_to_a_c_flag() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0xFF;
+        cpu.registers.c = 0x01;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x81);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.get_flag(CpuFlags::C), true);
+        assert_eq!(cpu.registers.get_flag(CpuFlags::H), true);
+    }
+
+    // ADD A, D
+    #[test]
+    fn test_add_d_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x10;
+        cpu.registers.d = 0x20;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x82);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x30);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    // ADD A, E
+    #[test]
+    fn test_add_e_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x01;
+        cpu.registers.e = 0x02;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x83);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x03);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    // ADD A, H
+    #[test]
+    fn test_add_h_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x05;
+        cpu.registers.h = 0x05;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x84);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x0A);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    // ADD A, L
+    #[test]
+    fn test_add_l_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x02;
+        cpu.registers.l = 0x03;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x85);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x05);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    // ADD A, (HL)
+    #[test]
+    fn test_add_hl_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x01;
+        cpu.registers.set_hl(0x1000);
+        cpu.bus.write_data(0x1000, 0x02);
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x86);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x03);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+
+    // ADD A, A
+    #[test]
+    fn test_add_a_to_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x03;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x87);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x06);
+        assert_eq!(cpu.registers.f, 0x0);
+    }
+    #[test]
+    fn test_sub_no_carry() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x0A;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x97);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.f, 0xC0); // Z and N flags set, H and C cleared
+    }
+
+    #[test]
+    fn test_sub_b_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0xF;
+        cpu.registers.b = 0x5;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x90);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0xA);
+        assert_eq!(cpu.registers.f, 0x40); // Z and N flags
+    }
+    #[test]
+    fn test_sub_c() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x8;
+        cpu.registers.c = 0x3;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x91);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x5);
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N set
+    }
+
+    #[test]
+    fn test_sub_d() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x7;
+        cpu.registers.d = 0x2;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x92);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x5);
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N set
+    }
+
+    #[test]
+    fn test_sub_e() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0xA;
+        cpu.registers.e = 0xA;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x93);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.f & 0xC0, 0xC0); // Z and N set
+    }
+
+    #[test]
+    fn test_sub_h() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0xF;
+        cpu.registers.h = 0x1;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x94);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0xE);
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N set
+    }
+
+    #[test]
+    fn test_sub_l() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x3;
+        cpu.registers.l = 0x3;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x95);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.f & 0xC0, 0xC0); // Z and N set
+    }
+
+    #[test]
+    fn test_sub_hl() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x5;
+        cpu.registers.h = 0x10;
+        cpu.registers.l = 0x00;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.get_hl(), 0x2); // memory at HL
+        cpu.bus.write_data(cpu.registers.pc, 0x96);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x3);
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N set
+    }
+
+    #[test]
+    fn test_sub_a() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x7;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x97);
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.f & 0xC0, 0xC0); // Z and N set
+    }
+    #[test]
+    fn test_sub_b_carry() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x05;
+        cpu.registers.b = 0x0A; // A < B -> carry set
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x90); // SUB B
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0xFB); // 5 - 10 = -5 = 0xFB
+        assert_eq!(cpu.registers.f & 0x10, 0x10); // C flag set
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N flag set
+    }
+
+    #[test]
+    fn test_sub_b_half_carry() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x10;
+        cpu.registers.b = 0x01; // borrow from bit 4
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x90); // SUB B
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x0F);
+        assert_eq!(cpu.registers.f & 0x20, 0x20); // H flag set
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N flag set
+        assert_eq!(cpu.registers.f & 0x10, 0x00); // C flag not set
+    }
+
+    #[test]
+    fn test_sub_a_no_carry_no_half() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x15;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.pc, 0x97); // SUB A
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0x00);
+        assert_eq!(cpu.registers.f & 0xC0, 0xC0); // Z and N set
+        assert_eq!(cpu.registers.f & 0x30, 0x00); // H and C cleared
+    }
+
+    #[test]
+    fn test_sub_hl_carry_half() {
+        let mut cpu = Cpu::new();
+        cpu.registers.a = 0x10;
+        cpu.registers.h = 0x00;
+        cpu.registers.l = 0x01;
+        cpu.registers.f = 0x00;
+        cpu.bus.write_data(cpu.registers.get_hl(), 0x11); // value > A
+        cpu.bus.write_data(cpu.registers.pc, 0x96); // SUB (HL)
+        cpu.step();
+        assert_eq!(cpu.registers.a, 0xFF); // 0x10 - 0x11 = -1 = 0xFF
+        assert_eq!(cpu.registers.f & 0x10, 0x10); // C set
+        assert_eq!(cpu.registers.f & 0x20, 0x20); // H set
+        assert_eq!(cpu.registers.f & 0x40, 0x40); // N set
+        assert_eq!(cpu.registers.f & 0x80, 0x00); // Z cleared
     }
 }
